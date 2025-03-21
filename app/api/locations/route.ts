354c0536d26d420bde42ea890d5535e17b215368
DenZaiyy <denzaiyy@outlook.fr>
2025-03-21 14:15:20 +0100

    fix: required fields

--- app/api/locations/route.ts
+++ app/api/locations/route.ts
@@ -3,6 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     const { name, description, longitude, latitude, address } = await req.json();
+
+    if (!name || !description || !longitude || !latitude || !address) {
+        return new NextResponse("Missing fields", { status: 400 });
+    }
+
     try {
         const location = await db.location.create({
             data: {
