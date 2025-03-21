b3c5832843efe0e2fde9d6093d8683fe6e6d103c
DenZaiyy <denzaiyy@outlook.fr>
2025-03-21 11:15:19 +0100

    feat: add locations route api

---
+++ app/api/locations/route.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const { name, description, longitude, latitude, address } = await req.json();
+    try {
+        const location = await db.location.create({
+            data: {
+                name: name,
+                description: description,
+                latitude: latitude,
+                longitude: longitude,
+                address: address
+            }
+        });
+
+        return NextResponse.json(location);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error("[LOCATION_POST] ", err.message);
+        }
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
