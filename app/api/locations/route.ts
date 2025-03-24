dc61e30ba926fe05960092dc7b6dd6bc1aae5eab
DenZaiyy <denzaiyy@outlook.fr>
2025-03-24 16:58:55 +0100

    feat: get all locations

--- app/api/locations/route.ts
+++ app/api/locations/route.ts
@@ -1,6 +1,27 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET() {
+    try {
+        const locations = await db.location.findMany({
+            orderBy: {
+                name: "asc"
+            }
+        });
+
+        if (!locations) {
+            return new NextResponse("Locations Not Found", { status: 404 });
+        }
+
+        return NextResponse.json(locations);
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error('[LOCATION_GET] ', err.message);
+        }
+        return new NextResponse('Internal Error', { status: 500});
+    }
+}
+
 export async function POST(req: NextRequest) {
     const { name, description, longitude, latitude, address } = await req.json();
 
