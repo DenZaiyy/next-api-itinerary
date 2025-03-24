24c6ab082fb70a401eca78c9a9964536c31ca2c0
DenZaiyy <denzaiyy@outlook.fr>
2025-03-24 17:01:01 +0100

    start: itinerary submission with locations (new systems)

--- app/api/itineraries/route.ts
+++ app/api/itineraries/route.ts
@@ -19,19 +19,65 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest) {
-    const { title, description } = await req.json();
+    const { title, description, locations } = await req.json();
+
+    console.log('Locations: ', locations)
 
     if (!title || !description) {
         return new NextResponse("Missing fields", { status: 400 });
     }
 
     try {
+        // TODO: Fix submission data with locations from location list
         const itinerary = await db.itinerary.create({
             data: {
                 title: title,
-                description: description
+                description: description,
+                LocationItinerary: {
+                    create: locations.map((location) => ({
+                        order: location.order,
+                        mustToSee: location.mustToSee || false,
+                        location: {
+                            connect: {
+                                id: location.location
+                            }
+                        }
+                    }))
+                }
+            },
+            include: {
+                LocationItinerary: {
+                    include: {
+                        location: true
+                    }
+                }
             }
         });
+
+        /*const itinerary = await db.itinerary.create({
+            data: {
+                title: title,
+                description: description,
+                LocationItinerary: {
+                    create: locations.map((location, index) => ({
+                        location: {
+                            create: {
+                                name: location.name,
+                                description: location.description,
+                                latitude: location.latitude,
+                                longitude: location.longitude,
+                                address: location.address
+                            }
+                        },
+                        order: index + 1,
+                        mustToSee: location.mustToSee
+                    }))
+                }
+            },
+            include: {
+                LocationItinerary: true
+            }
+        });*/
 
         return NextResponse.json(itinerary);
     } catch (err) {
